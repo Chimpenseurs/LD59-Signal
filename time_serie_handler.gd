9bf29b6c281c108f67df02133ce34a5950ccb43d
chtimy <jeremy.riffet@gmail.com>
2026-04-20 00:25:55 +0200

    cleanup

--- time_serie_handler.gd
+++ time_serie_handler.gd
@@ -5,28 +5,17 @@ extends Node
 
 signal set_pause
 
-var current_time = 0.0
-var total_time = 10.0
-@export var xy_scale = Vector2(1.0,1)
-@export var time_scale = 1.0
-var time_scale_init = time_scale
-#@export var initial_derivation_vec_coef = 1.0
-#@export var initial_derivation_timeleft = 0.5
-@export var dist_offset_threshold = 100
+# constants
+var baseline_h = 0.5
+var medium_h = 0.3
+var high_h = 0.1
 
-var slow_coef = 0.1
-var slow_time_scale = time_scale * slow_coef
+var current_time = 0.0
+@export var xy_scale = Vector2(1.0,1)
+var time_scale = 0.05
 
 var path_pixels_ref = 7839.0
 
-var previous_position : Vector2
-var derivation_vec : Vector2
-var derivation_vec_coef = 0.0
-var derivation_timeleft = 0.0
-
-# Let suppose an interval between 2 points is 20
-# If t = 33, the point before t is time_serie[1] and
-# we need to lerp at 13/20 from time_serie[1] to time_serie[2]
 var time_serie = []
 var triggers = []
 var velocities_scales = []
@@ -38,10 +27,6 @@ var valid_control = false
 
 func get_serie() -> Array[Vector2]:
 	return time_serie
-
-var baseline_h = 0.5
-var medium_h = 0.3
-var high_h = 0.1
 
 func inject_pulse(pulse, velocity_scale):
 	triggers.append(time_serie.size())
@@ -110,22 +95,10 @@ func _ready() -> void:
 		column.position.x = time_serie[triggers[i]].x-45
 		$Line2D.add_child(column)
 
-	previous_position = $Path2D/PathFollow2D/Circle.global_position
-	
 	# this allows to get the total number of pixels in the path, we need it for the speed scale
 	$Path2D/PathFollow2D.progress_ratio = 1.0
 	time_scale *= (path_pixels_ref / float($Path2D/PathFollow2D.progress))
-	time_scale_init = time_scale
 	$Path2D/PathFollow2D.progress_ratio = 0.0
-	
-	if not Engine.is_editor_hint():
-		var animation = $AnimationPlayer.get_animation("slower")
-		var track_id = animation.find_track(".:time_scale", Animation.TYPE_VALUE)
-
-		animation.track_set_key_value(track_id, 0, time_scale_init)
-		animation.track_set_key_value(track_id, 1, time_scale_init * 0.1)
-		animation.track_set_key_value(track_id, 2, time_scale_init)
-	
 	
 func _get_path_current_position():
 	return $Path2D.curve.sample_baked($Path2D/PathFollow2D.progress)
@@ -172,8 +145,6 @@ func _process(delta: float) -> void:
 			print("MISSED")
 			$AnimationPlayer.play("slower")
 		self.next_trigger_point_id+=1
-		
-	previous_position = player_pos
 
 func _on_trigger_area_entered(area: Area2D) -> void:
 	valid_control = true
