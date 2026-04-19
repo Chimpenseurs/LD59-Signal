09fca34b25ec5a7d77bc3aff29a6f7dd21376424
chtimy <jeremy.riffet@gmail.com>
2026-04-19 10:50:16 +0200

    up

--- time_serie_handler.gd
+++ time_serie_handler.gd
@@ -3,8 +3,8 @@ extends Node
 
 var current_time = 0.0
 var total_time = 10.0
-@export var xy_scale = Vector2(1,1)
-@export var time_scale = 0.1
+@export var xy_scale = Vector2(1.0,1)
+@export var time_scale = 1.0
 #@export var initial_derivation_vec_coef = 1.0
 #@export var initial_derivation_timeleft = 0.5
 @export var dist_offset_threshold = 100
@@ -21,6 +21,8 @@ var derivation_timeleft = 0.0
 # If t = 33, the point before t is time_serie[1] and
 # we need to lerp at 13/20 from time_serie[1] to time_serie[2]
 var time_serie = []
+var triggers = []
+var velocities_scales = []
 
 var next_point_id = 1
 
@@ -84,8 +86,37 @@ func get_serie() -> Array[Vector2]:
 	#points.push_back(last_point)
 	#return points
 	
-func add_pulse(offset_x, stride_x) -> Array[Vector2]:
-	return [Vector2(offset_x, 0.1), Vector2(offset_x + stride_x * 0.2, 0.9), Vector2(offset_x + stride_x * 0.5, 0.3), Vector2(offset_x + stride_x, 0.5)]
+var baseline_h = 0.5
+var medium_h = 0.3
+var high_h = 0.1
+
+func inject_pulse(pulse, velocity_scale):
+	triggers.append(time_serie.size())
+	scale_pulse(pulse)
+	time_serie.append_array(pulse)
+	for i in pulse.size()-1:
+		velocities_scales.append(velocity_scale)
+	velocities_scales.append(1)
+
+func scale_pulse(pulse):
+	for i in pulse.size():
+		pulse[i] *= xy_scale
+
+func length_pulse(pulse) -> float :
+	var length = 0
+	for i in pulse.size()-1:
+		length += pulse[i].distance_to(pulse[i+1])
+	return length
+
+func add_simple_pulse(offset_x, stride_x):
+	var pulse = [Vector2(offset_x, baseline_h), Vector2(offset_x + stride_x * 0.5, medium_h), Vector2(offset_x + stride_x * 0.9, baseline_h)]
+	var velocity_scale = stride_x / length_pulse(pulse)
+	inject_pulse(pulse, velocity_scale)
+
+func add_regular_pulse(offset_x, stride_x):
+	var pulse = [Vector2(offset_x, baseline_h), Vector2(offset_x + stride_x * 0.2, high_h), Vector2(offset_x + stride_x * 0.4, 1.0 - high_h), Vector2(offset_x + stride_x * 0.6, medium_h), Vector2(offset_x + stride_x * 0.9, 0.5)]
+	var velocity_scale = stride_x / length_pulse(pulse)
+	inject_pulse(pulse, velocity_scale)
 	
 func _ready() -> void:
 	xy_scale.y = get_viewport().get_visible_rect().size.y
@@ -94,10 +125,9 @@ func _ready() -> void:
 		var is_even = i % 2
 		var bound_y = 0.3 if is_even else 0.7
 		if i == 24:
-			time_serie.append_array(add_pulse(i * stride_x, stride_x))
-			i+=1
-		else :
-			time_serie.append(Vector2(i*stride_x, bound_y) * xy_scale)
+			add_regular_pulse(i * stride_x, stride_x)
+		elif is_even:
+			add_simple_pulse(i * stride_x, stride_x)
 		
 	$Line2D.points = time_serie
 	
@@ -106,12 +136,16 @@ func _ready() -> void:
 	for i in time_serie.size():
 		time_serie[i]
 		curve.add_point(time_serie[i])
-		var column = $Column.duplicate()
-		column.position.x = time_serie[i].x-45
-		$Line2D.add_child(column)
 	$Path2D.curve = curve
 	
+	for i in triggers.size():
+		var column = $Column.duplicate()
+		column.scale.x *= xy_scale.x
+		column.position.x = time_serie[triggers[i]].x-45
+		$Line2D.add_child(column)
+	
 	previous_position = $Path2D/PathFollow2D/Circle.global_position
+	$Path2D/PathFollow2D/Circle.material = load("res://materials/blooming_player.tres")
 
 #func _get_segment_points(path2D : Path2D) -> Array[Vector2]:
 	#for pt_i in time_serie.size():
@@ -130,20 +164,22 @@ func _get_path_next_position():
 	
 func _process(delta: float) -> void:
 	
-	current_time += delta * time_scale
+	var velocity_scale = velocities_scales[triggers[next_point_id]]
 	var player_pos = $Path2D/PathFollow2D/Circle.global_position
+	var next_point = time_serie[triggers[next_point_id]]
+	var distance_to_next_point = player_pos.distance_to(next_point)
+	
+	current_time += delta * (time_scale * velocity_scale)
 	
 	$Camera2D.position.x = _get_path_current_position().x
 	$Path2D/PathFollow2D.set_progress_ratio(current_time)
 	
-	var distance_to_next_point = player_pos.distance_to($Path2D.curve.get_point_position(next_point_id))
-	
-	if distance_to_next_point < dist_offset_threshold:
-		$Path2D/PathFollow2D/Circle.material = load("res://materials/blooming_player.tres")
-		$Path2D/PathFollow2D/Circle.scale = Vector2(1.5,1.5)
-	else:
-		$Path2D/PathFollow2D/Circle.material = null
-		$Path2D/PathFollow2D/Circle.scale = Vector2(0.5,0.5)
+	#if distance_to_next_point < dist_offset_threshold:
+		#$Path2D/PathFollow2D/Circle.material = load("res://materials/blooming_player.tres")
+		#$Path2D/PathFollow2D/Circle.scale = Vector2(1.5,1.5)
+	#else:
+		#$Path2D/PathFollow2D/Circle.material = null
+		#$Path2D/PathFollow2D/Circle.scale = Vector2(0.5,0.5)
 		
 	var has_pressed = false
 	if Input.is_action_just_pressed("ui_up"):
@@ -156,7 +192,7 @@ func _process(delta: float) -> void:
 			print("BAD")
 			$AnimationPlayer.play("slower")
 			
-	if player_pos.x > $Path2D.curve.get_point_position(next_point_id).x:
+	if player_pos.x > next_point.x:
 		if not has_pressed:
 			print("MISSED")
 			$AnimationPlayer.play("slower")
